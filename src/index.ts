cbfe1bfb386ffe5d80eb23a019aef39f7d13ad39
Brion Mario <info@brionmario.com>
2022-03-27 01:26:54 +0530

    chore(base): fix lint issues

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,9 @@
  */
 
 export const sum = (a: number, b: number) => {
-    if ("development" === process.env.NODE_ENV) {
-        // TODO: DO SOMETHING
-    }
+  if ("development" === process.env.NODE_ENV) {
+    // TODO: DO SOMETHING
+  }
 
-    return a + b;
+  return a + b;
 };
